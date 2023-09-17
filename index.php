93b3330c3ae7d2da49ef054fcf3f1307df23a833
Polo The Noob <marr40201@gmail.com>
2023-09-16 19:59:35 -0600

    Joins Y Query Producto

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+    require("conex.php");
+    echo "<H3>Lista De Productos</H3>"."<br>";
+    $query="SELECT * FROM producto";
+    $query1="SELECT pro.id_producto, pro.nombre, pro.precio, cat.nombre AS Categoria, inven.stock AS Stock 
+    FROM producto AS pro
+    JOIN categoria AS cat
+    ON pro.id_categoria = cat.id_categoria 
+    JOIN inventario AS inven
+    ON pro.id_inventario = inven.id_inventario";
+
+    echo "<a href='insert.php'> INSERT </a><P>";
+
+    if($result = $conex->query($query1)){
+        while ($row = $result->fetch_assoc()) {
+            $id = $row["id_producto"];
+            $categoria = $row["Categoria"];
+            $inventario = $row["Stock"];
+            $producto = $row["nombre"];
+            $precio = $row["precio"];
+            echo $id."".$producto."".$categoria."".$precio."".$inventario."<P>";
+        
+        }
+
+    }
